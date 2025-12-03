9cb02e26a90428ba519d0f94b511fe5af227a6ed
Revaz <revazgelashviliofficial@gmail.com>
2025-12-03 20:22:15 +0400

    change browserouter

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./styles/main.scss";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/CV">
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 );
